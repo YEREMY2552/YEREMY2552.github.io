2e13e6f358cf4ecceae1e32afa762d136f77828d
Fishyu Y. Mamani <fisiumamani55@gmail.com>
2026-04-28 23:05:56 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -12,7 +12,7 @@ const products = [
     },
     {
         id: 2,
-        name: "HOLA",
+        name: "Calzoles del boliviano",
         price: 45.00,
         category: "flores", // Asegúrate de que coincida con tus botones de filtro
         description: "Descripción de tu producto",
